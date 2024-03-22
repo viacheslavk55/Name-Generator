88151677f9536eda1096220a6b942f5f1e94e15c
viacheslavk55 <525vkostenko@frhsd.com>
2024-03-22 10:37:57 -0400

    made the add name button work

--- script.js
+++ script.js
@@ -8,3 +8,24 @@ function addName() {
     displayNames()
     nameInput.value = ''
 }
+
+function displayNames() {
+    const nameList = document.getElementById('nameList')
+    nameList.innerHTML = ''
+
+    for (let i = 0; i < namesArray.length; i++) {
+        const name = namesArray[i]
+
+        const li = document.createElement('li')
+        li.className = 'list-group-item'
+
+        const span = document.createElement('span')
+        span.textContent = name
+
+        li.appendChild(span)
+        nameList.appendChild(li)
+    }
+}
+
+
+document.getElementById('addNameBtn').addEventListener('click', addName)
